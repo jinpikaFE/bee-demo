d53f0de84dd84f5c84734ed5988d6b6c32f35cb7
jinx <847164495@qq.com>
2024-10-14 16:25:15 +0800

    chore: init api

--- routers/router.go
+++ routers/router.go
@@ -1,13 +1,30 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
 
 import (
-	"beego-demo/controllers"
-	"github.com/astaxie/beego"
+	"bee-demo/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
-	beego.Router("/api/provinces", &controllers.RegionController{}, "get:GetProvinces")
-    beego.Router("/api/cities", &controllers.RegionController{}, "get:GetCities")
-    beego.Router("/api/districts", &controllers.RegionController{}, "get:GetDistricts")
+	ns := beego.NewNamespace("/v1",
+		beego.NSNamespace("/object",
+			beego.NSInclude(
+				&controllers.ObjectController{},
+			),
+		),
+		beego.NSNamespace("/user",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
 }
