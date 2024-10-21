8d10d756f98a30e72be63d61578ce56e82a0c3c5
jinx <847164495@qq.com>
2024-10-21 16:28:12 +0800

    feat: 校验信息处理

--- filters/jwt_filter.go
+++ filters/jwt_filter.go
@@ -2,9 +2,10 @@ package filters
 
 import (
 	"bee-demo/utils"
-	"github.com/beego/beego/v2/server/web/context"
 	"net/http"
 	"strings"
+
+	"github.com/beego/beego/v2/server/web/context"
 
 	"github.com/beego/beego/v2/adapter/logs"
 )
@@ -14,7 +15,7 @@ func JwtFilter(ctx *context.Context) {
 	tokenString := ctx.Input.Header("Authorization")
 	if tokenString == "" {
 		ctx.Output.SetStatus(http.StatusUnauthorized)
-		ctx.Output.Body([]byte("缺少token"))
+		ctx.Output.JSON(map[string]interface{}{"msg": "缺少token", "code": 401, "data": nil}, true, true)
 		return
 	}
 
@@ -24,7 +25,7 @@ func JwtFilter(ctx *context.Context) {
 	if err != nil {
 		logs.Error("Token 验证失败:", err)
 		ctx.Output.SetStatus(http.StatusUnauthorized)
-		ctx.Output.Body([]byte("无效的token"))
+		ctx.Output.JSON(map[string]interface{}{"msg": "无效的token", "code": 401, "data": nil}, true, true)
 		return
 	}
 
